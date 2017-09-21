95c51a104c0c56da28182c005caa0564e6e3e559
Guillaume Schreiner <schreiner@unistra.fr>
2017-09-21 13:15:54 +0200

    fix CoAP er-rest-example in order to run with avr-zidguino default parameters

--- examples/er-rest-example/project-conf.h
+++ examples/er-rest-example/project-conf.h
@@ -37,8 +37,8 @@
 
 /* Disabling RDC for demo purposes. Core updates often require more memory. */
 /* For projects, optimize memory and enable RDC again. */
-#undef NETSTACK_CONF_RDC
-#define NETSTACK_CONF_RDC     nullrdc_driver
+//#undef NETSTACK_CONF_RDC
+//#define NETSTACK_CONF_RDC     nullrdc_driver
 
 /* Increase rpl-border-router IP-buffer when using more than 64. */
 #undef REST_MAX_CHUNK_SIZE
@@ -51,10 +51,8 @@
 */
 
 /* The IP buffer size must fit all other hops, in particular the border router. */
-/*
 #undef UIP_CONF_BUFFER_SIZE
 #define UIP_CONF_BUFFER_SIZE    1280
-*/
 
 /* Multiplies with chunk size, be aware of memory constraints. */
 #undef COAP_MAX_OPEN_TRANSACTIONS
@@ -74,9 +72,9 @@
 
 /* Save some memory for the sky platform. */
 #undef NBR_TABLE_CONF_MAX_NEIGHBORS
-#define NBR_TABLE_CONF_MAX_NEIGHBORS     10
+#define NBR_TABLE_CONF_MAX_NEIGHBORS     40
 #undef UIP_CONF_MAX_ROUTES
-#define UIP_CONF_MAX_ROUTES   10
+#define UIP_CONF_MAX_ROUTES   70
 
 /* Reduce 802.15.4 frame queue to save RAM. */
 #undef QUEUEBUF_CONF_NUM
--- platform/avr-zigduino/contiki-conf.h
+++ platform/avr-zigduino/contiki-conf.h
@@ -139,7 +139,7 @@ typedef unsigned short uip_stats_t;
 #define RIMEADDR_CONF_SIZE        8
 #define UIP_CONF_ICMP6            1
 #define UIP_CONF_UDP              1
-#define UIP_CONF_TCP              1
+//#define UIP_CONF_TCP              1
 #define NETSTACK_CONF_NETWORK     sicslowpan_driver
 #define SICSLOWPAN_CONF_COMPRESSION SICSLOWPAN_COMPRESSION_HC06
 #else
