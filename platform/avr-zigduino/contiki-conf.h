e123721ffc2665fbef196c0c18f706b97b546b0f
Guillaume Schreiner <schreiner@unistra.fr>
2017-09-21 13:12:48 +0200

    configure default network stack for avr-zidguino with nullmac + sicslowmac_driver + framer_802154

--- platform/avr-zigduino/contiki-conf.h
+++ platform/avr-zigduino/contiki-conf.h
@@ -178,8 +178,7 @@ typedef unsigned short uip_stats_t;
 #if 1 /* No radio cycling */
 
 #define NETSTACK_CONF_MAC         nullmac_driver
-// #define NETSTACK_CONF_RDC         sicslowmac_driver
-#define NETSTACK_CONF_RDC         nullrdc_driver
+#define NETSTACK_CONF_RDC         sicslowmac_driver
 #define NETSTACK_CONF_FRAMER      framer_802154
 #define NETSTACK_CONF_RADIO       rf230_driver
 #define CHANNEL_802_15_4          26
